3fffa013c2d9b3594b26a20d27e4954177a72300
ธนชาติ ชาวนาฝ้าย <614259044@webmail.npru.ac.th>
2020-10-05 11:33:55 +0700

    toey

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const dotenv = require('dotenv').config();
+const Unsplash = require('unsplash-js').default;
+const {toJson} = require('unsplash-js');
+
+//3. Configuration here
+const unsplash = new Unsplash({accessKey:process.env.myAccessKey});
+
+
+//4. add API here
+router.post('/api/searchPhotos', (req,res)=>{
+    //PUT YOUR CODE HERE
+    let {keyword} = req.body;
+    unsplash.search.photos(keyword,1,21)
+    .then(toJson)
+    .then(json =>{
+       res.status(200).send(json)
+    })
+    .catch((err)=>{
+        console.log("Error Message -->", err);
+        res.status(200).send({"messageError":err})
+    });
+
+});
+
+
+module.exports = router;
